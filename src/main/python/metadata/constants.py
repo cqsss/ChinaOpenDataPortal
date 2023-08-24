72163219b0a98c4e2a229da70d78cc5eb2d65c3c
5tRrn9 <stringcom@outlook.com>
2023-08-24 21:59:12 +0800

    modify saving path

--- src/main/python/metadata/constants.py
+++ src/main/python/metadata/constants.py
@@ -2,12 +2,12 @@
 DEFAULT_ENCODING = 'utf-8'
 REQUEST_TIME_OUT = 5
 
-PROVINCE_CURL_JSON_PATH = 'src\main\python\metadata\config\curl.json'
+PROVINCE_CURL_JSON_PATH = 'src/main/python/metadata/config/curl.json'
 
 PROVINCE_LIST = ['shandong', 'jiangsu']
 
 # METADATA_SAVE_PATH = 'src\\main\\python\\metadata\\json\\data\\'
-METADATA_SAVE_PATH = '/home/qschen/Data/ChinaOpenData/metadata/'
+METADATA_SAVE_PATH = 'src/main/python/metadata/data/metadata/'
 
 # MAPPING_SAVE_PATH = 'src\\main\\python\\metadata\\json\\mapping\\'
-MAPPING_SAVE_PATH = '/home/qschen/Data/ChinaOpenData/mapping/'
+MAPPING_SAVE_PATH = 'src/main/python/metadata/data/mapping/'
