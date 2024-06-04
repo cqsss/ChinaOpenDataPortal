d9c92f642069f1d89a56f2f3ded60fde4ae04cbb
stringmax <stringcom@outlook.com>
2024-06-04 15:15:24 +0800

    simple check for user-agent

---
+++ src/main/java/nju/websoft/chinaopendataportal/Config/WebConfig.java
@@ -0,0 +1,16 @@
+package nju.websoft.chinaopendataportal.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import nju.websoft.chinaopendataportal.Util.UserAgentInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new UserAgentInterceptor());
+    }
+}
